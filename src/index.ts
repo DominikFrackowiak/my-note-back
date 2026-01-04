03d403c2e517729fd2f9b0480d6e3185b8b20f96
DominikFrackowiak <rorschach@op.pl>
2026-01-04 12:08:01 +0100

    feat/env configuration

--- src/index.ts
+++ src/index.ts
@@ -1,1 +1,11 @@
-console.log('running on 3000')
+import express from 'express'
+
+import { PORT } from '../config/env.js'
+
+const app = express()
+
+app.get('/', (req, res) => {
+	res.send('Hello world')
+})
+
+app.listen(PORT, () => console.log(`App and running on port ${PORT}`))
